21549fcbcf5c93c2476046140a9c3dc958bfd5fe
Mário W. Oliveira <mario.wilyan@hotmail.com>
2016-04-12 14:16:40 -0300

    script pronto
    Script pronto

--- Exercicios/sitedosirmao/sitedosirmao/js/script.js
+++ Exercicios/sitedosirmao/sitedosirmao/js/script.js
@@ -26,21 +26,55 @@ function dialogo() {
 };
 
 function verifica(){
-	var teste = 0;
+	var testen = 0;
 	var validou = 0;
-	var nome = document.querySelector("#nome").value.split(' ');
-	var email = document.querySelector("#email").value.split('@');
-	
-	var fnome = nome[0];
-	var snome = nome[1];
-	var fmail = email[0];
-	var smail = email[1];
-	
-	if((fnome.length < 3) && (snome.length < 4)){
-		alert("Verificar campos obrigatórios");	
+
+	var nome = document.querySelector("#nome").value;
+	var email = document.querySelector("#email").value;
+
+	for (var i=0; i<nome.length; i++){
+		if(nome[i]== " "){
+		testen= 1;
+		}
 	}
-	
-	if((fmail.length < 3) && (smail.length < 4)){
-		alert("Verificar campos obrigatórios");	
+
+	for (var i=0; i<email.length; i++){
+		if(email[i]== "@"){
+		validou= 1;
+		}
 	}
+	if(testen==1){
+		var nome = document.querySelector("#nome").value.split(' ');
+		var fnome = nome[0];
+		var snome = nome[1];
+
+
+		if(fnome.length < 3){
+			var teste = 1;	
+		}
+		if(snome.length < 4){
+			var teste = 1;	
+		}
+	}else{
+			var teste = 1;	
+	 }
+
+	if(validou==1){	
+		var email = document.querySelector("#email").value.split('@');
+
+		var fmail = email[0];
+		var smail = email[1];
+
+		if(fmail.length < 3){
+			var teste = 1;	
+		}
+		if(smail.length < 4){
+			var teste = 1;		
+		}
+	}else{
+			var teste = 1;	
+	 }
+	 if(teste== 1){
+		alert("Verificar os campos obrigatorio!")	 
+	 }
 }
