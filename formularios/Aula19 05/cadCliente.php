43cabaf9130f393aa7dcba7a0c5fa47afb572b58
Mário W. Oliveira <mario.wilyan@hotmail.com>
2016-06-02 11:26:02 -0300

    Update cadCliente.php

--- formularios/Aula19 05/cadCliente.php
+++ formularios/Aula19 05/cadCliente.php
@@ -22,73 +22,84 @@
 			$login= $_POST["txtLogin"];
 			$senha1= $_POST["txtSenha1"];
 			$senha2= $_POST["txtSenha2"];
-			
-			$verificaData = true;
-			
+			$verificaData = true;			
 			$camposOK= true;
+			
+			if(isset($POST["checkCinema"])){
+				$cinema=true;
+			}else{
+				$cinema=false;
+			}
+			if(isset($POST["checkMusica"])){
+				$musica=true;
+			}else{
+				$musica=false;
+			}
+			if(isset($POST["checkInfo"])){
+				$info=true;
+			}else{
+				$info=false;
+			}
+			
 			if ($nome == ""){
 				echo"Informe o NOME.<br>";
 				$camposOK=false;
 			}
+			
 			if ($ender == ""){
 				echo"Informe o Endereço.<br>";
 				$camposOK=false;
 			}
+			
 			if ($senha1 != $senha2){
 				echo"As SENHAS não conferem.<br>";
 				$camposOK=false;
 			}
-			
+	//Verifica CPF	
 			function validaCPF($cpf = null) {
  
-    // Verifica se um número foi informado
-    if(empty($cpf)) {
-        return false;
-    }
+				if(empty($cpf)) {
+					return false;
+				}
  
-    // Elimina possivel mascara
-    $cpf = ereg_replace('[^0-9]', '', $cpf);
-    $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
-     
-    // Verifica se o numero de digitos informados é igual a 11 
-    if (strlen($cpf) != 11) {
-        return false;
-    }
-    // Verifica se nenhuma das sequências invalidas abaixo 
-    // foi digitada. Caso afirmativo, retorna falso
-    else if ($cpf == '00000000000' || 
-        $cpf == '11111111111' || 
-        $cpf == '22222222222' || 
-        $cpf == '33333333333' || 
-        $cpf == '44444444444' || 
-        $cpf == '55555555555' || 
-        $cpf == '66666666666' || 
-        $cpf == '77777777777' || 
-        $cpf == '88888888888' || 
-        $cpf == '99999999999') {
-        return false;
-     // Calcula os digitos verificadores para verificar se o
-     // CPF é válido
-     } else {   
+			$cpf = ereg_replace('[^0-9]', '', $cpf);
+			$cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+		  
+			if (strlen($cpf) != 11) {
+				return false;
+			}
+			else if ($cpf == '00000000000' || 
+					$cpf == '11111111111' || 
+					$cpf == '22222222222' || 
+					$cpf == '33333333333' || 
+					$cpf == '44444444444' || 
+					$cpf == '55555555555' || 
+					$cpf == '66666666666' || 
+					$cpf == '77777777777' || 
+					$cpf == '88888888888' || 
+					$cpf == '99999999999') {
+					return false;
+		} else {   
          
-        for ($t = 9; $t < 11; $t++) {
+			for ($t = 9; $t < 11; $t++) {
              
-            for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf{$c} * (($t + 1) - $c);
-            }
-            $d = ((10 * $d) % 11) % 10;
-            if ($cpf{$c} != $d) {
-                return false;
-            }
-        }
+				for ($d = 0, $c = 0; $c < $t; $c++) {
+					$d += $cpf{$c} * (($t + 1) - $c);
+				}
+				$d = ((10 * $d) % 11) % 10;
+				if ($cpf{$c} != $d) {
+					return false;
+				}
+			}
  
-        return true;
-    }
-}
-         if(!validaCPF($cpf)){
-			 echo"CPF INVÁLIDO<br>";
-			 $camposOK=false; 
+			return true;
+		}
+	}
+     if(!validaCPF($cpf)){
+		echo"<b>CPF INVÁLIDO</b><br>";
+		$camposOK=false; 
 	 }
+	// Verifica DATA
 	if($dtNasc == ""){
 		$verificaData = false;
 	}
